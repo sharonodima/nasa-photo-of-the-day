fde09bd179cc7b5e1f7740a87ec57883538d300f
Sharon Odima <odimasharon@gmail.com>
2022-04-21 11:23:31 -0400

    Sharon odima Completed Week2Module3 and Module4

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import {ThemeProvider} from "styled-components"
-import theme from "./theme";
-import {render} from "react-dom"
 
-render (
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-    ,
-    document.querySelector("#root")
-  )
-ReactDOM.render(<App />, document.getElementById("root"));
